6a2a78f566fb03330112b646f03cc2922bd27eb4
Ananda Vijaya <anandaprabu.trityavijaya@gmail.com>
2021-12-15 11:52:00 +0700

    more vector exercise, including memory management

--- STL Container/vector.cpp
+++ STL Container/vector.cpp
@@ -4,26 +4,58 @@
 
 using namespace std;
 
+void printVector(vector<int> vec) {
+    if(vec.empty()) {
+        cout << "Vector Empty" << "\n\n";
+        return;
+    }
+    
+    for(vector<int>::iterator itr=vec.begin(); itr!=vec.end(); itr++) {
+        cout << *itr << " ";
+    }
+    cout << "\n\n";
+}
+
 int main() {
     vector<int> vec;
     int n; cin>>n;
     REP(i,0,n) {
         int x; cin>>x;
-        vec.push_back(x*i);
+        vec.push_back(x*2);
     }
+    cout << "Vec: ";
+    printVector(vec);
 
-    for(auto i:vec) cout << i << " ";
-    cout<<"\n";
-
-    vector<int>::iterator vecItr;
-    for (vecItr=vec.begin(); vecItr!=vec.end(); vecItr++) {
-        cout<<*vecItr<<" ";
-    }
-    cout<<"\n";
-
+    cout << "Vec (Reversed): ";
     for (auto vecRitr=vec.rbegin(); vecRitr!=vec.rend(); vecRitr++) {
         cout<<*vecRitr<<" ";
-    }
+    } cout<<endl;
+
+    vector<int> vec2;
+    vec2.insert(vec2.begin(),{2,1,3,4,5});
+    cout << "Vec2: ";
+    printVector(vec2);
+
+    vec==vec2? cout<<"Vec == Vec2" : cout<<"Vec != Vec2";
+    cout<<endl<<endl;
+    vec<vec2? cout<<"Vec < Vec2" : cout<<"Vec >= Vec2"; // only compare the first element?
+
+    cout << endl << "Capacity Vec:" << vec.capacity();
+    vec.shrink_to_fit();
+    cout << endl << "Capacity Vec After Shrink:" << vec.capacity();
+    cout<< endl << "Capacity Vec2: " << vec2.capacity();
+    vec.swap(vec2);
+    cout << endl;
+    cout << endl << "Capacity Vec:" << vec.capacity();
+    cout<< endl << "Capacity Vec2: " << vec2.capacity();
+
+    vec2.resize(16);
+    cout<< endl << "Capacity Vec2: " << vec2.capacity();
+    cout<<endl;
+    vec2.shrink_to_fit();
+    cout<< endl << "Capacity Vec2: " << vec2.capacity();
+    cout<<endl;
+    printVector(vec2);
 
     return 0;
 }
